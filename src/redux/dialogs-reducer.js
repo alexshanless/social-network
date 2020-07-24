e17100933b9d14d6eed8f942181d00bed76a06e9
Alex Bilba <alex.shanless@gmail.com>
2020-07-23 21:49:44 -0700

    dial red

--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -22,18 +22,19 @@ let initialState = {
 };
 
 const dialogsReducer = (state = initialState, action) => {
-  let stateCopy = { ...state };
   switch (action.type) {
-    case UPDATE_NEW_MESSAGE_BODY: {
-      stateCopy.newMessageBody = action.body;
-      return stateCopy;
-    }
-    case SEND_MESSAGE: {
-      let body = stateCopy.newMessageBody;
-      stateCopy.newMessageBody = "";
-      stateCopy.messages.push({ id: 7, message: body });
-      return stateCopy;
-    }
+    case UPDATE_NEW_MESSAGE_BODY:
+      return {
+        ...state,
+        newMessageBody: action.body,
+      };
+    case SEND_MESSAGE:
+      let body = state.newMessageBody;
+      return {
+        ...state,
+        newMessageBody: "",
+        messages: [...state.messages, { id: 7, message: body }],
+      };
     default:
       return state;
   }
