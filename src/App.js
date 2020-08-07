d499d8c5bf42f53701c7afa0a8621cf6f029c6a4
Alex Bilba <alex.shanless@gmail.com>
2020-08-06 20:46:21 -0700

    auth/me

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React from "react";
 import "./App.css";
-import Header from "./components/Header/Header";
+import o from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import { Route } from "react-router-dom";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/users/UsersContainer";
-import ProfileContainer from './components/Profile/ProfileContainer';
-
+import ProfileContainer from "./components/Profile/ProfileContainer";
+import HeaderContainer from './components/Header/HeaderContainer';
 
 function App(props) {
   return (
     <div className="app-wrapper">
-      <Header />
+      <HeaderContainer />
       <Navbar />
       <div className="app-wrapper-content">
         <Route path="/dialogs" render={() => <DialogsContainer />} />
         <Route path="/profile/:userId?" render={() => <ProfileContainer />} />
-        <Route path="/users" render={() => <UsersContainer/>} />
+        <Route path="/users" render={() => <UsersContainer />} />
       </div>
     </div>
   );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import s from "./Header.module.css";
+import { NavLink } from "react-router-dom";
 
-const Header = () => {
+const Header = (props) => {
   return (
     <header className={s.header}>
       <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSJubvac5LtNOwJtYNtNx6m67AKuexoQ3KxfZJxjtdkVQPaaN0c&usqp=CAU" />
+
+      <div className={s.loginBlock}>
+        {props.isAuth ? props.login
+       : <NavLink to={"/login"}>Login</NavLink>}
+      </div>
     </header>
   );
 };
