feaf1d8d595557df87d0182d068e76929ed84cd5
Alex Bilba <alex.shanless@gmail.com>
2020-08-26 20:43:44 -0700

    DAL

---
+++ src/api/api.js
@@ -0,0 +1,19 @@
+import * as axios from "axios";
+
+const instance = axios.create({
+  withCredentials: true,
+  baseURL: `https://social-network.samuraijs.com/api/1.0/`,
+  headers: {
+    "API-KEY": "5a27546d-25a4-4655-a5b8-e3af55cbfda8",
+  },
+});
+
+export const usersAPI = {
+  getUsers(currentPage, pageSize) {
+    return instance
+      .get(`users?page=${currentPage}&count=${pageSize}`)
+      .then((response) => {
+        return response.data;
+      });
+  },
+};
