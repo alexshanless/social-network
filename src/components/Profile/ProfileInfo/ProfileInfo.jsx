cd7c855ccd4fc786dc9b233050141df23607d2a7
Alex Bilba <alex.shanless@gmail.com>
2020-10-07 21:09:00 -0700

    redux devtools

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import s from "./ProfileInfo.module.css";
 import Preloader from "../../common/Preloader";
-import ProfileStatus from "./ProfileStatus";
-import ProfileStatusWithHooks from './ProfileStatusWithHooks';
+import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
 
 const ProfileInfo = (props) => {
   if (!props.profile) {
@@ -11,12 +10,12 @@ const ProfileInfo = (props) => {
 
   return (
     <div>
-      {/* <div>
-        <img src="https://image.shutterstock.com/image-photo/bright-spring-view-cameo-island-260nw-1048185397.jpg" />
-      </div> */}
       <div className={s.descriptionBlock}>
         <img src={props.profile.photos.large} />
-        <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus} />
+        <ProfileStatusWithHooks
+          status={props.status}
+          updateStatus={props.updateStatus}
+        />
         Description
       </div>
     </div>
--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -2,11 +2,9 @@ import React from "react";
 import styles from "./users.module.css";
 import userPhoto from "../../assests/images/avatar.png";
 import { NavLink } from "react-router-dom";
-import { usersAPI } from "./../../api/api";
 import Paginator from "../common/Paginator/Paginator";
 
 let Users = (props) => {
-
   return (
     <div>
       <Paginator
