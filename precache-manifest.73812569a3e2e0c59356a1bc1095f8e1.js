a4b297cd07fc9800560c9d0e7cddf179105c8cb9
Alex Bilba <alex.shanless@gmail.com>
2020-10-11 23:09:54 -0700

    Updates

---
+++ precache-manifest.73812569a3e2e0c59356a1bc1095f8e1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61828cd649d6f0c18927488e0e6d4f14",
+    "url": "/social-network-react/index.html"
+  },
+  {
+    "revision": "93df2f20c91747c16cb2",
+    "url": "/social-network-react/static/css/main.f40f1f8f.chunk.css"
+  },
+  {
+    "revision": "92f9f07192adcab4314d",
+    "url": "/social-network-react/static/js/2.fba37dd9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/social-network-react/static/js/2.fba37dd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93df2f20c91747c16cb2",
+    "url": "/social-network-react/static/js/main.6c0411c0.chunk.js"
+  },
+  {
+    "revision": "7e86cd35e9038ec15f2d",
+    "url": "/social-network-react/static/js/runtime-main.e32b629c.js"
+  },
+  {
+    "revision": "7c8e5f50725885d51b72b45164274e41",
+    "url": "/social-network-react/static/media/avatar.7c8e5f50.png"
+  },
+  {
+    "revision": "3f2590067056b4f0630d5b360e694fce",
+    "url": "/social-network-react/static/media/loader.3f259006.gif"
+  }
+]);
